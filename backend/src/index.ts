14659f233b3ba6fe983261cbb2594d32759f2954
Manju <manju@Manjus-MacBook-Air.local>
2026-03-03 16:00:42 +0530

    error check on the workflow

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,5 +15,5 @@ app.use("/api/auth", authPayload)
 app.use(express.json())
 
 app.listen(process.env.PORT, () => {
-    console.info(`Server is running on port ${process.env.PORT}`)
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
