a2149a9929e9c0d0c395317f866aac2bb3267fc2
Manju0810 <vmanju810@gmail.com>
2026-03-09 11:29:17 +0530

    refactor/add unit tests for register endpoit (#11)
    Co-authored-by: Manju <manju@Manjus-MacBook-Air.local>

---
+++ backend/src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import authPayload from './routes/auth';
+import cookieParser from 'cookie-parser';
+
+export const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'server is running' });
+});
+
+app.use('/api/auth', authPayload);
